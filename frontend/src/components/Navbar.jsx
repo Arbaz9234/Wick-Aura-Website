47d83d3711d1e74febbd360b62282f7988840cb6
arbaz9234 <arbaz9234@gmail.com>
2026-05-07 23:41:22 +0530

    updated navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,9 +5,9 @@ import { Search, User } from "lucide-react";
 export default function Navbar() {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <a href="/">
+      <NavLink to="/">
         <img src={assets.logoHome} alt="Wick & Aura Logo" className="w-36" />
-      </a>
+      </NavLink>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
