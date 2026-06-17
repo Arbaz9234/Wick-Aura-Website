67c947d93b0350674132bf95a30937aa4d18cae3
arbaz9234 <arbaz9234@gmail.com>
2026-06-17 18:25:09 +0530

    Product page completed styling pending

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
+import { Star } from "lucide-react";
 
 export default function Product() {
   const { productId } = useParams();
-  const { products } = React.useContext(ShopContext);
+  const { products, currency, addToCart } = React.useContext(ShopContext);
   const [productData, setProductData] = React.useState(false);
   const [image, setImage] = React.useState("");
-
+  const [size, setSize] = React.useState("");
   const fetchProductData = async () => {
     products.map((product) => {
       if (product._id === productId) {
@@ -39,13 +40,71 @@ export default function Product() {
             ))}
           </div>
         </div>
-        {/* <div className="md:w-1/2">
-          <h1 className="text-3xl font-bold">{productData.name}</h1>
-          <p className="text-xl text-gray-700 mt-4">
-            ${productData.price.toFixed(2)}
+      </div>
+      {/* -------- Product Info ---------- */}
+      <div className="flex-1">
+        <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+        <div className=" flex items-center gap-1 mt-2">
+          <Star className="w-3.5" />
+          <Star className="w-3.5" />
+          <p className="pl-2">(122)</p>
+        </div>
+        <p className="mt-5 text-3xl font-medium">
+          {currency}
+          {productData.price}
+        </p>
+        <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
+        <div className="flex flex-col gap-4 my-8">
+          <p>Select Size</p>
+          <div className="flex gap-2">
+            {productData.sizes.map((item, index) => (
+              <button
+                onClick={() => setSize(item)}
+                className={`border py-2 px-4 bg-gray-100 ${item === size ? "border-orange-500" : ""}`}
+                key={index}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
+        </div>
+        <button
+          onClick={() => addToCart(productData._id, size)}
+          className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
+        >
+          ADD TO CART
+        </button>
+        <hr className="mt-8 sm:w-4/5" />
+        <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+          <p>100% Original product.</p>
+          <p>Cash on delivery is available on this product.</p>
+          <p>Easy return and exchange policy within 7 days.</p>
+        </div>
+      </div>
+
+      {/* ---------- Description & Review Section ------------- */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            An e-commerce website is an online platform that facilitates the
+            buying and selling of products or services over the internet. It
+            serves as a virtual marketplace where businesses and individuals can
+            showcase their products, interact with customers, and conduct
+            transactions without the need for a physical presence. E-commerce
+            websites have gained immense popularity due to their convenience,
+            accessibility, and the global reach they offer.
           </p>
-          <p className="text-gray-600 mt-4">{productData.description}</p>
-        </div> */}
+          <p>
+            E-commerce websites typically display products or services along
+            with detailed descriptions, images, prices, and any available
+            variations (e.g., sizes, colors). Each product usually has its own
+            dedicated page with relevant information.
+          </p>
+        </div>
       </div>
     </div>
   ) : (
